25ce973031c191debe776900f871d3d7de23e97e
mvpjeff88@gmail.com <mvpjeff88@gmail.com@56662eda-5599-0469-5f10-f2ef13ce4079>
2011-04-07 06:07:52 +0000

    -- Jeff - 2:07 AM - 4/7 --
    - Added a drop-zone row at the top of the screen so you can see the row that you're potentially dropping in

--- trunk/ConnectFour/src/ConnectFourGUI.java
+++ trunk/ConnectFour/src/ConnectFourGUI.java
@@ -3,30 +3,52 @@ import javax.swing.border.*;
 import java.awt.*; 
 import java.io.File;
 
+
 public class ConnectFourGUI { 
    
   private JFrame frame; 
+  private JLabel[] drops;
   private JLabel[][] slots; 
   private int currentPlayer;   
-  private int xSize = 700;
-  private int ySize = 600;
+  private int xSize;
+  private int ySize;
+  private int currentCol = 0;
+  
+  private final int NUM_ROWS = 6;
+  private final int NUM_COLS = 7;
   
   public ConnectFourGUI() { 
-     
     frame = new JFrame("Connect Four"); 
+    
+    xSize = (NUM_ROWS+1) * 100; // this will set the sizes based on the number of rows
+    ySize = NUM_COLS * 100; // this will set the sizes based on the number of rows
+    
     JPanel panel = (JPanel) frame.getContentPane(); 
-    panel.setLayout(new GridLayout(6,7)); 
-    slots = new JLabel[7][6]; 
-    for (int row=5; row>=0; row--) { 
-      for (int column=0; column<7; column++) { 
+    panel.setLayout(new GridLayout(NUM_ROWS+1,NUM_COLS)); 
+    
+    // set the drop zone slots
+    drops = new JLabel[NUM_COLS];
+    for (int col = 0; col < NUM_COLS; col++)
+    {
+    	drops[col] = new JLabel(); 
+        
+    	drops[col].setHorizontalAlignment(SwingConstants.CENTER); 
+    	drops[col].setBorder(new LineBorder(Color.red)); 
+        panel.add(drops[col]); 
+    }
+    
+    // set the inner slots
+    slots = new JLabel[NUM_COLS][NUM_ROWS]; 
+    for (int row = NUM_ROWS-1; row >= 0; row--) { //going this way so jlabels added correctly
+      for (int column = 0; column < NUM_COLS; column++) { 
         slots[column][row] = new JLabel(); 
-         // slots[column][row].setFont(new Font("SansSerif", Font.BOLD, 18)); 
                     
         slots[column][row].setHorizontalAlignment(SwingConstants.CENTER); 
         slots[column][row].setBorder(new LineBorder(Color.green)); 
         panel.add(slots[column][row]); 
       }      
     }    
+    
     Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     frame.setContentPane(panel); 
     frame.setSize(xSize,ySize); 
@@ -37,22 +59,32 @@ public class ConnectFourGUI {
   } 
    
   public void addListener(ConnectFourListener listener) { 
-    for (int row=0; row<6; row++) { 
-      for (int column=0; column<7; column++) { 
+	for (int column = 0; column < NUM_COLS; column++) { 
+	  drops[column].addMouseListener(listener); 
+	}   
+	  
+	for (int row = 0; row < NUM_ROWS; row++) { 
+      for (int column = 0; column < NUM_COLS; column++) { 
         slots[column][row].addMouseListener(listener); 
       }      
     }  
   }
-  public int getColumn(JLabel label) { 
-    int returnColumn = -1; 
-    for (int row=0; row<6; row++) { 
-      for (int column=0; column<7; column++) { 
+  public int getColumn(JLabel label) {
+	for (int col = 0; col < NUM_COLS; col++)
+	  {
+	  	if (drops[col] == label)
+	  		return col;
+	  }
+	
+    for (int row = 0; row < NUM_ROWS; row++) { 
+      for (int column = 0; column < NUM_COLS; column++) { 
         if (slots[column][row] == label) { 
-          returnColumn = column; 
+          return column; 
         }        
       }      
     }
-    return returnColumn; 
+    
+    return -1; 
   }  
    
 public void set(int column, int row) { 
@@ -80,7 +112,42 @@ public void set(int column, int row) {
     }
     
     currentPlayer = (currentPlayer%2)+1; 
+    
+    mouseInColumn(currentCol);
  } 
+
+	public void mouseInColumn(int column)
+	{
+		if (column < 0 || column >= NUM_COLS)
+			return;
+		
+		drops[currentCol].setIcon(new ImageIcon());
+		
+		currentCol = column;
+		
+		String path = new String();
+	    if (currentPlayer == 1) { 
+	    	path = "Images/RedChip.png";
+	    }    
+	    else { 
+	      path = "Images/BlackChip.png";
+	    }   
+	    
+	    java.net.URL imgURL = getClass().getResource(path);
+	    if (imgURL != null) {
+	    	ImageIcon originalImageIcon = new ImageIcon(imgURL);
+	    	Image initialImage = originalImageIcon.getImage();
+	    	Image resizedImage = initialImage.getScaledInstance(90, 90,  java.awt.Image.SCALE_SMOOTH);
+	    	
+	    	ImageIcon image = new ImageIcon(resizedImage);
+	    	if (image != null) {
+	    		drops[currentCol].setIcon(image);
+	    	}
+	    } else {
+	        System.err.println("Couldn't find file: " + path);
+	    }
+	    
+	}
    
   public static void main(String[] args) { 
 	SplashScreen splash = new SplashScreen(5000);
